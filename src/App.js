98c4d8ea6d1e166f9c1b73c6eadfbf4e9670bca1
JohnyTheLittle <76442434+JohnyTheLittle@users.noreply.github.com>
2024-01-25 23:58:20 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css'
+import Form from './components/Form/Form'
 import Header from './components/Header/Header'
-import { useTelegram } from './hooks/useTelegram'
+import ProdList from './components/ProdList/ProdList'
+import { Route, Routes } from 'react-router-dom'
+
+
 function App() {
-  const { onToggleButton } = useTelegram()
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={<ProdList/>}/>
+        <Route path={'form'} element={<Form/>}/>
+      </Routes>
     </div>
   )
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Form.css'
+import { useTelegram } from '../../hooks/useTelegram'
 
 const Form = () => {
-    return (
-        <div>
-            Form
-        </div>
-    )
+  const [street, setStreet] = useState('')
+  const [city, setCity] = useState('')
+  const [subject, setSubject] = useState('endless_horror')
+  const { tg } = useTelegram()
+
+  useEffect(() => {}, [])
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: 'Отправить данные',
+    })
+  }, [])
+
+  useEffect(() => {
+    if (!street || !city) {
+      tg.MainButton.hide()
+    } else {
+      tg.MainButton.show()
+    }
+  }, [tg.MainButton, street, city])
+
+  const onChangeCity = (e) => {
+    setCity(e.target.value)
+  }
+  const onChangeStreet = (e) => {
+    setStreet(e.target.value)
+  }
+  const onChangeSubject = (e) => {
+    setSubject(e.target.value)
+  }
+
+  return (
+    <div className="form">
+      <h3>Input your data</h3>
+      <input
+        className="input"
+        type="text"
+        placeholder={'City'}
+        value={city}
+        onChange={onChangeCity}
+      />
+      <input
+        type="text"
+        placeholder={'Street'}
+        value={street}
+        onChange={onChangeStreet}
+      />
+      <select className="select" onSelect={onChangeSubject} value={subject}>
+        <option value={'horrible_end'}>Horrible End</option>
+        <option value={'endless_horror'}>Endless Horror</option>
+      </select>
+    </div>
+  )
 }
 
 export default Form
