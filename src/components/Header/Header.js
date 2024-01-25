cc99870ae117544d86a9286a30669f876a8d86e4
JohnyTheLittle <76442434+JohnyTheLittle@users.noreply.github.com>
2024-01-26 00:29:52 +0300

    1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import Button from '../Buttons/Button'
 import { useTelegram } from '../../hooks/useTelegram'
-import "./Header.css"
+
+import './Header.css'
+import { useNavigate } from 'react-router-dom'
 
 const Header = (props) => {
   const { user, onClose } = useTelegram()
+  const navigate = useNavigate()
+  const handleClick = (routeName) => {
+    navigate(routeName)
+  }
   return (
     <div className="header">
       <Button onClick={onClose}>Закрыть</Button>
       <span {...props} className={'user'}>
         {user?.first_name}
       </span>
+      <Button onClick={() => {handleClick('')}}>Prod List</Button>
+      <Button onClick={() => {handleClick('/form')}}>Form</Button>
     </div>
   )
 }
